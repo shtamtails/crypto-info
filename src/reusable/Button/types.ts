10a9527f6352a9195ac1f97fef4249800757ef77
shtamtails <maximyudin1@gmail.com>
2023-04-08 21:59:00 +0300

    new button variant - danger

--- src/reusable/Button/types.ts
+++ src/reusable/Button/types.ts
@@ -3,7 +3,7 @@ import { defaultProps } from "../../models/defaultProps";
 export interface ButtonProps extends defaultProps {
   children: JSX.Element | string;
   fullWidth?: boolean;
-  variant?: "regular" | "outline";
+  variant?: "regular" | "outline" | "danger";
   className?: string;
   onClick?: () => any;
 }
