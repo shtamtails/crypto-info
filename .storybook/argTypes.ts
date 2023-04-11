7d7c682e253792303ed86a0e8e30f7a590b05f3f
shtamtails <maximyudin1@gmail.com>
2023-04-12 00:31:12 +0300

    CI-4 Add Input component to storybook

--- .storybook/argTypes.ts
+++ .storybook/argTypes.ts
@@ -1,27 +1,39 @@
 export const defaultArgs = {
   pl: {
     description: "padding-left",
+    control: { type: "select" },
   },
   pr: {
     description: "padding-right",
+    control: { type: "select" },
   },
   pt: {
     description: "padding-top",
+    control: { type: "select" },
   },
   pb: {
     description: "padding-bottom",
+    control: { type: "select" },
   },
   mt: {
     description: "margin-top",
+    control: { type: "select" },
   },
   mb: {
     description: "margin-bottom",
+    control: { type: "select" },
   },
   ml: {
     description: "margin-left",
+    control: { type: "select" },
   },
   mr: {
     description: "margin-right",
+    control: { type: "select" },
+  },
+  radius: {
+    description: "Border radius of the component",
+    control: { type: "select" },
   },
   style: {
     description: "Custom css properties",
