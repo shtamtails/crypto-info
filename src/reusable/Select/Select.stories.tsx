0d6e6e394bd1350350cb2963b1d994e000b997c9
shtamtails <maximyudin1@gmail.com>
2023-04-12 03:01:00 +0300

    CI-6 Add Select component to storybook

---
+++ src/reusable/Select/Select.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Select } from "./Select";
+import "../../style/utils.scss";
+import { defaultArgs } from "../../../.storybook/argTypes";
+import "../Input/input.scss";
+
+const meta: Meta<typeof Select> = {
+  title: "UI/Select",
+  component: Select,
+  tags: ["autodocs"],
+  argTypes: {
+    ...defaultArgs,
+    value: {
+      description:
+        "A value, that represents the current state of the component.",
+    },
+    setValue: {
+      description: "A function that changes the state of component",
+    },
+    icon: {
+      description: `Icon component. F.e. we can use react-icons package and pass FiCircle component as the value`,
+    },
+    error: {
+      description: "Text that displayed below the input with red text color",
+    },
+    data: {
+      description: "Data in format [ { label: 'value', value: 'value' } ]",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+  args: {
+    label: "The best framework",
+    fullWidth: true,
+    data: [
+      { label: "React", value: "react" },
+      { label: "Angular", value: "angular" },
+      { label: "Vue", value: "vue" },
+    ],
+  },
+};
