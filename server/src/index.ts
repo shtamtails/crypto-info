a7009a7a14ba8de9acb93ae80b90407a3ddbe348
shtamtails <maximyudin1@gmail.com>
2023-04-19 22:16:03 +0300

    remove cors origin to deploy server on netflify

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "./_app";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 app.listen(4000);
