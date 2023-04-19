d3312ea0b686a82579c2f2d5202d8714a56a05b7
shtamtails <maximyudin1@gmail.com>
2023-04-19 22:21:26 +0300

    change back origin to localhost

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,7 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "./_app";
 
 const app = express();
-// app.use(cors({ origin: "http://localhost:3000" }));
-app.use(
-  cors({ origin: "https://main--marvelous-kashata-23b3f5.netlify.app/" })
-);
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 app.listen(4000);
