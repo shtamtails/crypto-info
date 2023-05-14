ea50d26df1a3db3da1cbc9029ee392c372e368fc
shtamtails <maximyudin1@gmail.com>
2023-05-14 19:43:08 +0300

    add tests for getDefaultClassName

--- client/src/utils/getDefaultClassName/getDefaultClassName.test.ts
+++ client/src/utils/getDefaultClassName/getDefaultClassName.test.ts
@@ -1,22 +1,93 @@
-// import { getDefaultClassName } from "./getDefaultClassName";
+import { getDefaultClassName } from "./getDefaultClassName";
 
-// describe("getDefaultClassName() should work fine", () => {
-//   test("Get Default ClassName should return correct className", () => {
-//     const defaultClassName = ["button"];
+describe("getDefaultClassName", () => {
+  it("should return correct classNames", () => {
+    expect(
+      getDefaultClassName({
+        props: { fullWidth: true },
+        defaultClassName: "defaultClassName",
+        withIndents: false,
+      })
+    ).toBe("defaultClassName fullWidth");
 
-//     expect(getDefaultClassName({ pl: "sm" }, defaultClassName)).toBe(
-//       "button padding-left-sm"
-//     );
+    expect(
+      getDefaultClassName({
+        props: { radius: "sm" },
+        defaultClassName: "",
+        withIndents: false,
+      })
+    ).toBe("border-radius-sm");
 
-//     expect(getDefaultClassName({ radius: "sm" })).toBe("border-radius-sm");
-//     expect(getDefaultClassName({ fullWidth: true })).toBe("fullWidth");
-//     expect(getDefaultClassName({ pl: "sm" })).toBe("padding-left-sm");
-//     expect(getDefaultClassName({ pr: "sm" })).toBe("padding-right-sm");
-//     expect(getDefaultClassName({ pt: "sm" })).toBe("padding-top-sm");
-//     expect(getDefaultClassName({ pb: "sm" })).toBe("padding-bottom-sm");
-//     expect(getDefaultClassName({ ml: "sm" })).toBe("margin-left-sm");
-//     expect(getDefaultClassName({ mr: "sm" })).toBe("margin-right-sm");
-//     expect(getDefaultClassName({ mt: "sm" })).toBe("margin-top-sm");
-//     expect(getDefaultClassName({ mb: "sm" })).toBe("margin-bottom-sm");
-//   });
-// });
+    expect(
+      getDefaultClassName({
+        props: { fullWidth: true },
+        defaultClassName: "",
+        withIndents: false,
+      })
+    ).toBe("fullWidth");
+
+    expect(
+      getDefaultClassName({
+        props: { pl: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("padding-left-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { pr: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("padding-right-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { pt: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("padding-top-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { pb: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("padding-bottom-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { ml: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("margin-left-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { mr: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("margin-right-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { mt: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("margin-top-sm");
+
+    expect(
+      getDefaultClassName({
+        props: { mb: "sm" },
+        defaultClassName: "",
+        withIndents: true,
+      })
+    ).toBe("margin-bottom-sm");
+  });
+});
