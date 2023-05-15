66506bd1fb6bb29980fd231dfa47c88b8e49e79c
shtamtails <maximyudin1@gmail.com>
2023-05-15 05:01:08 +0300

    useClickOutside hook

--- client/src/hooks/useClickOutside/useClickOutside.ts
+++ client/src/hooks/useClickOutside/useClickOutside.ts
@@ -1,19 +1,21 @@
 import { RefObject, useEffect } from "react";
 
+/**
+ *
+ * Hook that listens for clicks outside a given component and calls a callback function when it happens
+ * @param {React.RefObject<HTMLButtonElement | HTMLDivElement>} ref - Reference to the component being clicked outside of
+ * @param {Function} callBack - Callback function to be called when a click outside of the component occurs
+ * @returns {void}
+ *
+ */
+
 export const useClickOutside = (
-  ref: RefObject<any>,
-  parentElementRef: RefObject<any>,
-  callBack: Function
+  ref: RefObject<HTMLButtonElement | HTMLDivElement>,
+  callBack: () => void
 ) => {
   useEffect(() => {
     const clickHandler = (e: MouseEvent) => {
-      if (
-        ref.current &&
-        e.target &&
-        !ref.current.contains(e.target) &&
-        parentElementRef &&
-        !parentElementRef.current.contains(e.target)
-      ) {
+      if (ref.current && e.target && !ref.current.contains(e.target as Node)) {
         callBack();
       }
     };
@@ -21,5 +23,5 @@ export const useClickOutside = (
     () => {
       document.removeEventListener("mousedown", clickHandler);
     };
-  }, [ref]);
+  }, [ref, callBack]);
 };
